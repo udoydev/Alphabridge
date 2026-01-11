730e46e14cc11075e3c1a23d83f7b5af7a19488b
udoydev <githublinkedin@gmail.com>
2026-01-11 22:09:54 +0600

    Fix ALLOWED_HOSTS for Vercel

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8_1^1r=u1$vh&+#oa^pk0gn$17zqgi$qw*%i(77f8tsk3jzu03
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
